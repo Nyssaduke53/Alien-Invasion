aa2025010d7c7c2d953e3ef4ba61d40f1303d727
Nyssaduke53 <160543686+Nyssaduke53@users.noreply.github.com>
2025-04-16 12:55:59 -0400

    na

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 class Settings:
     """A class to store all settings for Alien Invasion."""
 
@@ -5,7 +6,7 @@ class Settings:
         """Initialize the game's settings."""
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_file = Path.cwd() / 'Assets' / 'images' / ''
 
         # Ship settings
         self.ship_speed = 4.5
